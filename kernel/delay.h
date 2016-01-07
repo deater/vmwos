64932677cf3a7c915de6a5c6ea71e12154671070
Vince Weaver <vincent.weaver@maine.edu>
2016-01-07 12:52:05 -0500

    vmwos: delay was broken with gcc-4.9
    Quite possibly my inline assembly was broken.
    Who can tell, making the paramater an input/output seems to have
    fixed things.

--- kernel/delay.h
+++ kernel/delay.h
@@ -14,9 +14,15 @@
 /* can use %[X] to refer to clobbere reg X */
 /* that can then use [X]"r"(x) to map to C variable */
 
+
+/* GRRRRR this broke moving from gcc-4.8.3 to gcc-4.9.3 */
+/* moving count to be input/output instead of just input seems to help */
+
 static inline void delay(int32_t count) {
         asm volatile("__delay_%=: subs %[count], %[count], #1; "
 			"bne __delay_%=\n"
-                 : : [count]"r"(count) : "cc");
+		: [count]"+r"(count) /* outputs */
+		: /* inputs */
+		: "cc" /* clobbers */);
 }
 
