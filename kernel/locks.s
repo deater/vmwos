d6c7925bcef7ed35ab7330bab2c798833c1b3033
Vince Weaver <vincent.weaver@maine.edu>
2016-01-08 12:54:28 -0500

    vmwos: locks: update comments

--- kernel/locks.s
+++ kernel/locks.s
@@ -10,14 +10,16 @@
 @ extern void lock_mutex(void *mutex);
 
 lock_mutex:
-	ldr	r1,=locked
+	ldr	r1,=locked	@ Load 1 into r1
 lock_retry:
-	ldrex	r2,[r0]
-	cmp	r2,r1
-	beq	lock_busy
-	strexne	r2,r1,[r0]
-	cmpne	r2,#1
-	beq	lock_retry
+	ldrex	r2,[r0]		@ load/mark exclusive from mutex
+	cmp	r2,r1		@ is it locked?
+	beq	lock_busy	@ if so, busy
+	strexne	r2,r1,[r0]	@ conditionally store the value in r1 to r0
+				@ (i.e. "locked" to the mutex)
+				@ r2 indicates whether this worked or not
+	cmpne	r2,#1		@ check to see if it worked
+	beq	lock_retry	@ if it failed, then retry
 
 	@ lock was acquired
 
@@ -26,11 +28,13 @@ lock_retry:
 	mov	r3,#0
 	mcr	p15,0,r3,c7,c10,5
 
-	bx	lr
+	bx	lr		@ return
 
 lock_busy:
-@	wait_for_update
+				@ Ideally we would do something low-power
+				@ besides just busy-waiting here
 	b	lock_retry
+
 
 @ unlock_mutex
 @ extern void unlock_mutex (void *mutex);
@@ -45,8 +49,9 @@ unlock_mutex:
 	mov	r3,#0
 	mcr	p15,0,r3,c7,c10,5
 
-	str	r1,[r0]
-@	signal_udpate
+	str	r1,[r0]			@ clear the lock
+@	signal_udpate			@ more advanced systems
+					@ we would wake sleepers here
 	bx	lr
 
 
