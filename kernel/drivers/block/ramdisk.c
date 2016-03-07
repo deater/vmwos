1cc3514db212872c90c9f43b423d68e117cf7b3c
Vince Weaver <vincent.weaver@maine.edu>
2016-03-07 13:38:10 -0500

    kernel: ramdisk_read wasn't returning errors properly

--- kernel/drivers/block/ramdisk.c
+++ kernel/drivers/block/ramdisk.c
@@ -12,11 +12,11 @@ struct ramdisk_info_t {
 	uint32_t flags;
 } ramdisk_info;
 
-uint32_t ramdisk_read(uint32_t offset, uint32_t length, char *dest) {
+int32_t ramdisk_read(uint32_t offset, uint32_t length, char *dest) {
 
 	/* Make sure we are in range */
 	if (offset+length>ramdisk_info.length) {
-		return ERANGE;
+		return -ERANGE;
 	}
 
 	memcpy(dest,ramdisk_info.start+offset,length);
@@ -25,39 +25,7 @@ uint32_t ramdisk_read(uint32_t offset, uint32_t length, char *dest) {
 
 }
 
-/* read NUL-terminated string */
-/* FIXME: verify we got the ranges right */
-uint32_t ramdisk_read_string(uint32_t offset, uint32_t maxlen, char *dest) {
-
-	uint32_t ptr=offset;
-	char *output=dest;
-	char ch;
-	uint32_t length=0;
-
-	while(1) {
-		ch=*(ramdisk_info.start+ptr);
-
-		/* We hit the end of the ramdisk */
-		if (
-			(ch==0) ||
-			(length>=maxlen) ||
-			(offset+length>ramdisk_info.length)) {
-			*output=0;
-			return 0;
-		}
-
-		*output=ch;
-
-		ptr++;
-		output++;
-		length++;
-	}
-
-	return 0;
-
-}
-
-uint32_t ramdisk_init(unsigned char *start, uint32_t length) {
+int32_t ramdisk_init(unsigned char *start, uint32_t length) {
 
 	printk("Initializing ramdisk of size %d at address %x\n",
 		length,start);
