ee01d6c249a42127a36762e5c86a1dafdf242665
Vince Weaver <vincent.weaver@maine.edu>
2016-05-09 18:58:39 -0400

    kernel: wait: rethinking blocking I/O

--- kernel/wait.c
+++ kernel/wait.c
@@ -8,6 +8,11 @@
 
 int32_t waitpid(int32_t pid, int32_t *wstatus, int32_t options) {
 
+	if (process[pid].status!=PROCESS_STATUS_EXITED) {
+		printk("Waiting on pid %d\n",pid);
+//		process[current_process].status=PROCESS_STATUS_SLEEPING;
+	}
+
 	if (wstatus!=NULL) *wstatus=process[current_process].exit_value;
 
 	/* Kill the zombie now? */
