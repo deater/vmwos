4585e045132f8258e24de86d366c52ab3514d426
Vince Weaver <vincent.weaver@maine.edu>
2016-05-09 14:45:32 -0400

    kernel: scheduler: don't reschedule if new proc is same as old one

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -46,14 +46,14 @@ void schedule(long *irq_stack) {
 	}
 
 	/* switch to new process */
-	if (i!=current_process) printk("Switching from %d to %d\n",current_process,i);
+	if (i!=current_process) {
+		printk("Switching from %d to %d\n",current_process,i);
+		process_run(i,irq_stack);
+	}
 
 	/* ARM documentation says we can put a */
 	/* clrex instruction here */
 	/* to avoid false positives in the mutex handling? */
 	/* if there's any aliasing between new and old process? */
-
-	/* IRQ stack */
-	process_run(i,irq_stack);
 
 }
