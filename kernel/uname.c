f86b1f8eb329ae7cd70c2c4bcbcdae29f0d29b2f
Vince Weaver <vincent.weaver@maine.edu>
2016-05-17 14:21:56 -0400

    kernel: support the uname() "version" field
    for now just stick the output of date into it

--- kernel/uname.c
+++ kernel/uname.c
@@ -5,11 +5,14 @@
 #include "uname.h"
 #include "lib/string.h"
 
+#include "date.h"
+
 int32_t uname(struct utsname *buf) {
 
 	strncpy(buf->sysname,"vmwOS",5);
 	strncpy(buf->nodename,"pi",2);
 	strncpy(buf->release,VERSION,strlen(VERSION));
+	strncpy(buf->version,COMPILE_DATE,65);
 	strncpy(buf->machine,"arm6",3);
 	strncpy(buf->domainname,"none",4);
 
