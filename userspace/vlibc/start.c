315dbf9f33243721870a3fa928380c6ab8c7056f
Vince Weaver <vincent.weaver@maine.edu>
2016-05-19 11:56:17 -0400

    userspace: use some linker magic to ensure _start is at beginning of exes

--- userspace/vlibc/start.c
+++ userspace/vlibc/start.c
@@ -1,7 +1,9 @@
 int main(int argc, char **argv, char **envp);
 void exit(int status);
 
-void _start(int argc, char **argv, char **envp) {
+void _start (int argc, char **argv, char **envp) __attribute__ ((section (".text.boot")));
+
+void _start (int argc, char **argv, char **envp) {
 
 	int result;
 
