5cc83c96da4b8b61953b73e039053d6441bc8506
Vince Weaver <vincent.weaver@maine.edu>
2016-10-31 12:50:07 -0400

    kernel: wait queue debug and fixes
    try to sort out the wait queue problem, which might not have
    been a problem in the end

--- kernel/wait.c
+++ kernel/wait.c
@@ -15,14 +15,18 @@ http://www.makelinux.net/ldd3/chp-6-sect-2
 #include "scheduler.h"
 #include "wait.h"
 
+#include "lib/printk.h"
+
 int32_t wait_queue_add(struct wait_queue_t *queue, struct process_control_block_type *proc) {
 
 	struct process_control_block_type *next;
 
 	if (queue->first==NULL) {
+//		printk("NULL: Adding %d to wait queue\n",proc->pid);
 		queue->first=proc;
 	}
 	else {
+//		printk("Adding %d to wait queue\n",proc->pid);
 		next=queue->first;
 		while(1) {
 			if (next->wait_queue_next==NULL) {
@@ -32,8 +36,10 @@ int32_t wait_queue_add(struct wait_queue_t *queue, struct process_control_block_
 			next=next->wait_queue_next;
 		}
 	}
+//	printk("Putting %d to sleep\n",proc->pid);
 	proc->status=PROCESS_STATUS_SLEEPING;
-	proc->wait_queue_next=NULL;
+	// why did we do this?
+//	proc->wait_queue_next=NULL;
 	schedule();
 
 	return 0;
@@ -41,13 +47,15 @@ int32_t wait_queue_add(struct wait_queue_t *queue, struct process_control_block_
 
 int32_t wait_queue_wake(struct wait_queue_t *queue) {
 
-	struct process_control_block_type *proc;
+	struct process_control_block_type *proc,*next_proc;
 
 	proc=queue->first;
 
 	while(proc!=NULL) {
 		proc->status=PROCESS_STATUS_READY;
-		proc=proc->wait_queue_next;
+		next_proc=proc->wait_queue_next;
+		proc->wait_queue_next=NULL;
+		proc=next_proc;
 	}
 	queue->first=NULL;
 
