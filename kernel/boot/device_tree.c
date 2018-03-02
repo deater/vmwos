6ea8196667dc1fc34fa5b4515f599376fe4483fb
Vince Weaver <vincent.weaver@maine.edu>
2018-03-02 16:16:18 -0500

    kernel: device-tree, more device-tree work

--- kernel/boot/device_tree.c
+++ kernel/boot/device_tree.c
@@ -1,15 +1,27 @@
+/* This code decodes the flattened device tree provided by the bootloader */
+
+/* See Section 5 of the "Devicetree Specification Release v0.2" */
+/* From devicetree.org */
+
 /* Original references */
 /* "Device Trees Everywhere" by Gibson and Herrenschmidt */
 
+#define FDT_BEGIN_NODE	1
+#define FDT_END_NODE	2
+#define FDT_PROP	3
+#define FDT_NOP		4
+#define FDT_END		9
 
-#define OF_DT_BEGIN_NODE	1
-#define OF_DT_PROP		3
-#define OF_DT_END_NODE		2
-#define OF_DT_END		9
-
+#include <inttypes.h>
 #include <stdint.h>
 
-#include "hardware.h"
+/* Not sure why arm cross-compiler does not support this? */
+#ifndef	PRIx64
+#define PRIx64	"%llx"
+#endif
+
+#ifndef STANDALONE_TEST
+
 #include "lib/string.h"
 #include "lib/memset.h"
 #include "lib/memcpy.h"
@@ -19,6 +31,21 @@
 #include "lib/printk.h"
 
 #include "boot/device_tree.h"
+
+#else
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define printk printf
+
+#endif
+
+static int fdt_verbose=0;
 
 struct device_tree_type {
 	void *address;
@@ -102,55 +129,118 @@ int32_t devicetree_decode(uint32_t *dt_ptr) {
 
 }
 
-uint32_t dt_parse_prop(uint32_t *tree, int *i) {
+static void dt_print_string(uint32_t offset) {
 
-	uint32_t temp;
+	char *string;
+	string=(char *)(device_tree.address+device_tree.off_strs+offset);
+
+	printk("%s",string);
+}
+
+static uint32_t dt_parse_prop(uint32_t *tree, int *i) {
+
+	uint32_t temp,length,j,k;
 	char string[5];
+	int printing,saved;
 
 	string[4]=0;
 
+	printk("\t");
+
 	temp=big_to_little(tree[*i]);
-	printk("\tstring addr: %x\n",temp);
+	if (fdt_verbose) printk("\tValue Length: %x\n",temp);
+	length=temp;
 	(*i)++;
 
 	temp=big_to_little(tree[*i]);
-	printk("\tLength: %x\n",temp);
+	if (fdt_verbose) printk("\tstring offset: %x\n",temp);
+	dt_print_string(temp);
 	(*i)++;
 
 	printk("\t");
-	while(1) {
+	printing=1;
+
+	saved=*i;
+
+	for(j=0;j<(length+3)/4;j++) {
+
 		temp=tree[*i];
 		(*i)++;
 		memcpy(string,&temp,4);
-		printk("%d %d %d %d\n",string[0],string[1],string[2],string[3]);
-		if (string[3]==0) break;
+
+		if (printing) {
+			for(k=0;k<4;k++) {
+				if (string[k]==0) {
+					printing=0;
+					break;
+				}
+				else {
+					printk("%c",string[k]);
+				}
+			}
+		}
 	}
+	printk("\n\t");
+
+	*i=saved;
+
+	for(j=0;j<(length+3)/4;j++) {
+
+		temp=tree[*i];
+		(*i)++;
+		memcpy(string,&temp,4);
+
+		for(k=0;k<4;k++) {
+			printk("%x",string[k]&0xff);
+		}
+	}
+	printk("\n");
+
 
 	return 0;
 }
 
-uint32_t dt_parse_node(uint32_t *tree, int *i) {
+static uint32_t dt_parse_node(uint32_t *tree, int *i) {
 
 	uint32_t temp;
+	char *ptr;
 
-	if (big_to_little(tree[*i])!=OF_DT_BEGIN_NODE) {
-		printk("Expected OF_DT_BEGIN_NODE, got %x\n",tree[*i]);
+	if (big_to_little(tree[*i])!=FDT_BEGIN_NODE) {
+		printk("Expected FDT_BEGIN_NODE, got %x\n",tree[*i]);
 		(*i)++;
 		return -1;
 	}
 	(*i)++;
 
-	temp=big_to_little(tree[*i]);
-	printk("Node name: %x\n",temp);
-	(*i)++;
+	printk("Node: ");
+
+	ptr=(char *)&tree[*i];
+	printk("%s\n",ptr);
+
+	/* Skip to end of padding */
+	while(1) {
+		temp=big_to_little(tree[*i]);
+		(*i)++;
+//		printk("%x ",temp);
+		if ((temp&0xff)==0) break;
+	}
+
 
 	while(1) {
 
-		if (big_to_little(tree[*i])==OF_DT_PROP) {
+		if (big_to_little(tree[*i])==FDT_NOP) {
+			(*i)++;
+			continue;
+		}
+		if (big_to_little(tree[*i])==FDT_PROP) {
 			(*i)++;
 			dt_parse_prop(tree,i);
 		}
-		else if (big_to_little(tree[*i])==OF_DT_END_NODE) {
+		else if (big_to_little(tree[*i])==FDT_BEGIN_NODE) {
+			dt_parse_node(tree,i);
+		}
+		else if (big_to_little(tree[*i])==FDT_END_NODE) {
+			if (fdt_verbose) printk("FDT_END_NODE\n");
 			(*i)++;
 			break;
 		} else {
@@ -171,11 +261,11 @@ void devicetree_dump(void) {
 	if (!device_tree.valid) return;
 
 	printk("Device tree:\n");
-	printk("\tMagic: %x\n",device_tree.magic);
+	printk("\tMagic: 0x%x\n",device_tree.magic);
 	printk("\tTotalsize:  %d\n",device_tree.totalsize);
-	printk("\tOff Struct: %x\n",device_tree.off_struct);
-	printk("\tOff strs:   %x\n",device_tree.off_strs);
-	printk("\tOff rsvmap: %x\n",device_tree.off_rsvmap);
+	printk("\tOff Struct: 0x%x\n",device_tree.off_struct);
+	printk("\tOff strs:   0x%x\n",device_tree.off_strs);
+	printk("\tOff rsvmap: 0x%x\n",device_tree.off_rsvmap);
 	printk("\tVersion:    %d\n",device_tree.version);
 	printk("\tLastCompVer:%d\n",device_tree.last_comp_ver);
 	if (device_tree.version>=2) {
@@ -192,8 +282,11 @@ void devicetree_dump(void) {
 
 	i=0;
 	while(1) {
-		if ((memory_reserved[i]==0) && (memory_reserved[i+1]==0)) break;
-		printk("\t0x%llx bytes starting at address 0x%llx\n",
+		if ((memory_reserved[i]==0) &&
+			(memory_reserved[i+1]==0)) break;
+
+		printk("\t0x%" PRIx64 " bytes starting at "
+			"address 0x%" PRIx64 "\n",
 			big_to_little64(memory_reserved[i+1]),
 			big_to_little64(memory_reserved[i]));
 		i+=2;
@@ -207,14 +300,13 @@ void devicetree_dump(void) {
 		dt_parse_node(tree,&i);
 
 		/* See if at end */
-		if (big_to_little(tree[i])==OF_DT_END) break;
+		if (big_to_little(tree[i])==FDT_END) break;
 		if (device_tree.off_struct + (i*4) > device_tree.totalsize) {
 			break;
 		}
 	}
 }
 
-#if 1
 
 int32_t devicetree_raw_dump(uint32_t *dt_ptr) {
 
@@ -248,6 +340,40 @@ int32_t devicetree_raw_dump(uint32_t *dt_ptr) {
 
 	return 0;
 
+}
+
+#ifdef STANDALONE_TEST
+
+#define MAXSIZE 100*1024
+
+int main(int argc, char **argv) {
+
+	char *dt_mem;
+	int fd;
+	int size;
+
+	dt_mem=calloc(MAXSIZE,sizeof(char));
+	if (dt_mem==NULL) {
+		fprintf(stderr,"Error opening!\n");
+		return -1;
+	}
+
+	fd=open("pi3.dt",O_RDONLY);
+	if (fd<0) {
+		fprintf(stderr,"ERROR opening pi3.dt\n");
+		return -1;
+	}
+
+	size=read(fd,dt_mem,MAXSIZE);
+
+	printf("Read %d bytes\n",size);
+
+	close(fd);
+
+	devicetree_decode((uint32_t *)dt_mem);
+	devicetree_dump();
+
+	return 0;
 }
 
 
