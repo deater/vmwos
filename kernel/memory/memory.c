1ddeda35ceace39d2011931f777e81a099be8a5b
Vince Weaver <vincent.weaver@maine.edu>
2018-03-15 15:10:54 -0400

    kernel: implement memory freeing

--- kernel/memory/memory.c
+++ kernel/memory/memory.c
@@ -8,9 +8,12 @@
 #include "memory/memory.h"
 
 #include "lib/printk.h"
+#include "lib/memset.h"
 
-#define MAX_MEMORY	(1024*1024*1024)
+#define MAX_MEMORY	(1024*1024*1024)		// 1GB
 #define CHUNK_SIZE	4096
+
+static int memory_debug=0;
 
 static unsigned int memory_total;
 
@@ -18,6 +21,7 @@ static unsigned int memory_map[MAX_MEMORY/CHUNK_SIZE/32];
 
 static unsigned int max_chunk=0;
 
+/* Mark a chunk as used (1) */
 static int memory_mark_used(int chunk) {
 
 	int element,bit;
@@ -30,6 +34,20 @@ static int memory_mark_used(int chunk) {
 	return 0;
 }
 
+/* Mark a chunk as free (0) */
+static int memory_mark_free(int chunk) {
+
+	int element,bit;
+
+	element=chunk/32;
+	bit=chunk%32;
+
+	memory_map[element]&=~(1<<bit);
+
+	return 0;
+}
+
+/* See if a chunk is used */
 static int memory_test_used(int chunk) {
 
 	int element,bit;
@@ -40,6 +58,7 @@ static int memory_test_used(int chunk) {
 	return memory_map[element] & (1<<bit);
 }
 
+/* Initialize memory */
 static int memory_init(unsigned long memory_total,unsigned long memory_kernel) {
 
 	int i;
@@ -70,6 +89,7 @@ static int memory_init(unsigned long memory_total,unsigned long memory_kernel) {
 	return 0;
 }
 
+/* Find a free chunk of memory */
 static int find_free(int num_chunks) {
 
 	int i,j;
@@ -88,6 +108,7 @@ static int find_free(int num_chunks) {
 	return -1;
 }
 
+/* Report amount of memory free */
 int32_t memory_total_free(void) {
 
 	int32_t total_free=0,i;
@@ -100,17 +121,25 @@ int32_t memory_total_free(void) {
 
 }
 
+/* allocate an area of memory */
+/* rounds up to nearest chunk size */
 void *memory_allocate(uint32_t size) {
 
 	int first_chunk;
 	int num_chunks;
 	int i;
 
-//	printk("Allocating memory of size %d bytes\n",size);
+	if (memory_debug) {
+		printk("Allocating memory of size %d bytes\n",size);
+	}
 
 	if (size==0) size=1;
 
 	num_chunks = ((size-1)/CHUNK_SIZE)+1;
+
+	if (memory_debug) {
+		printk("\tRounding up to %d %d chunks\n",num_chunks,CHUNK_SIZE);
+	}
 
 	first_chunk=find_free(num_chunks);
 
@@ -123,7 +152,10 @@ void *memory_allocate(uint32_t size) {
 		memory_mark_used(first_chunk+i);
 	}
 
-//	printk("MEM: Allocated %d bytes at %x\n",size,first_chunk*CHUNK_SIZE);
+	if (memory_debug) {
+		printk("MEM: Allocated %d bytes at %x\n",
+			size,first_chunk*CHUNK_SIZE);
+	}
 
 	return (void *)(first_chunk*CHUNK_SIZE);
 
@@ -131,7 +163,40 @@ void *memory_allocate(uint32_t size) {
 
 int32_t memory_free(void *location, uint32_t size) {
 
-	printk("ERROR: memory_free not implemented yet.\n");
+	int i;
+	int num_chunks;
+	int first_chunk;
+
+	if (memory_debug ) {
+		int32_t total;
+
+		total=memory_total_free();
+		printk("Free before: %d\n",total);
+	}
+
+	num_chunks = ((size-1)/CHUNK_SIZE)+1;
+
+	if (memory_debug) {
+		printk("Freeing %d bytes (%d CHUNKS) at %x\n",
+			size,num_chunks,location);
+	}
+
+	first_chunk=(int)location/CHUNK_SIZE;
+
+	/* Poison old memory */
+	memset(location,'V',size);
+
+	for(i=0;i<num_chunks;i++) {
+		memory_mark_free(first_chunk+i);
+	}
+
+	if (memory_debug ) {
+		int32_t total;
+
+		total=memory_total_free();
+		printk("Free after: %d\n",total);
+	}
+
 
 	return 0;
 }
