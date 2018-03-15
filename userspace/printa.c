d14a72da936418c52402e4f4b8fb160e03e76358
Vince Weaver <vincent.weaver@maine.edu>
2018-03-15 12:38:51 -0400

    userspace: update printa/printb to end and give more info

--- userspace/printa.c
+++ userspace/printa.c
@@ -1,9 +1,19 @@
+/* Prints A for 10s */
+/* Half of the traditional duo used to show multi-tasking is working */
+
 #include <stddef.h>
 #include <stdint.h>
 #include "syscalls.h"
 #include "vlibc.h"
 
+#define RUNTIME	10
+
 int main(int argc, char **argv) {
+
+	int start_time,current_time;
+	struct tms buf;
+
+	start_time=time(NULL);
 
 	while(1) {
 		printf("A");
@@ -13,5 +23,15 @@ int main(int argc, char **argv) {
 			"subs	r1,r1,#1\n"
 			"bne	a_loop\n"
 			:::);
+		current_time=time(NULL);
+		if (current_time-start_time>RUNTIME) break;
 	}
+
+	times(&buf);
+
+	printf("\nTime running A: "
+		"Wallclock: %d seconds, User: %d jiffies = %d seconds\n",
+		current_time-start_time,buf.tms_utime,buf.tms_utime/64);
+
+	return 0;
 }
