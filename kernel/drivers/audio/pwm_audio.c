b221dbcd19fd5597ac70d87bad86a2ca35bd4af7
Vince Weaver <vincent.weaver@maine.edu>
2019-10-15 17:02:05 -0400

    audio: more work on audio driver
    earlier checkins where in wrong tree, but I guess that's OK

--- kernel/drivers/audio/pwm_audio.c
+++ kernel/drivers/audio/pwm_audio.c
@@ -2,6 +2,7 @@
 /* which is PWM GPIO40/45 and a filter connected to the 3.5mm jack */
 
 /* For example see https://github.com/PeterLemon/RaspberryPi/blob/master/Sound/PWM/14Bit/44100Hz/Mono/DMA/kernel.asm */
+/* Also see the PiFox code */
 
 #include <stddef.h>
 #include <stdint.h>
@@ -10,9 +11,9 @@
 #include "drivers/bcm2835/bcm2835_periph.h"
 #include "drivers/bcm2835/bcm2835_io.h"
 #include "lib/printk.h"
+#include "lib/delay.h"
 
-
-#include "goodwork.h"
+//#include "goodwork.h"
 
 struct dma_control_structure {
 	uint32_t	transfer_information;
@@ -27,12 +28,12 @@ struct dma_control_structure {
 
 static struct dma_control_structure
 	__attribute__ ((aligned (32))) cb_struct1;
-static struct dma_control_structure
-	__attribute__ ((aligned (32))) cb_struct2;
+//static struct dma_control_structure
+//	__attribute__ ((aligned (32))) cb_struct2;
 
 
 static uint32_t __attribute__ ((aligned(16))) dma_buffer_0[0x2000];
-static uint32_t __attribute__ ((aligned(16))) dma_buffer_1[0x2000];
+//static uint32_t __attribute__ ((aligned(16))) dma_buffer_1[0x2000];
 
 
 uint32_t audio_pwm_init(void) {
@@ -41,8 +42,9 @@ uint32_t audio_pwm_init(void) {
 	/* copy audio into buffers */
 	/* Input is 8-bit, dma_buffer is 64 bit */
 	for(i=0;i<0x2000;i++) {
-		dma_buffer_0[i]=goodwork_bin[i];
-		dma_buffer_1[i]=goodwork_bin[i+0x2000];
+//		dma_buffer_0[i]=goodwork_bin[i];
+//		dma_buffer_1[i]=goodwork_bin[i+0x2000];
+		dma_buffer_0[i]=(i%256)<128?0:255;
 	}
 
 	/* setup csbs */
@@ -51,10 +53,11 @@ uint32_t audio_pwm_init(void) {
 	cb_struct1.destination_address=0x7E20C018;
 	cb_struct1.transfer_length=0x8000;
 	cb_struct1.twod_stride=0;
-	cb_struct1.next_control_block=(uint32_t)&cb_struct2;
+	cb_struct1.next_control_block=(uint32_t)&cb_struct1;
 	cb_struct1.padding0=0;
 	cb_struct1.padding1=0;
 
+#if 0
 	/* setup csbs */
 	cb_struct2.transfer_information=0x00050141;
 	cb_struct2.source_address=(uint32_t)&dma_buffer_1;
@@ -64,7 +67,7 @@ uint32_t audio_pwm_init(void) {
 	cb_struct2.next_control_block=(uint32_t)&cb_struct2;
 	cb_struct2.padding0=0;
 	cb_struct2.padding1=0;
-
+#endif
 
 	/* Setup GPIO pins 14 and 15 */
 	gpio_request(40,"pwm0");
@@ -78,10 +81,10 @@ uint32_t audio_pwm_init(void) {
 	/* Bits 0..11 Fractional Part Of Divisor = 0 */
 	/* Bits 12..23 Integer Part Of Divisor = 2 */
 	bcm2835_write(CM_PWMDIV, CM_PASSWORD + 0x2000);
-//	bcm2835_write(CM_PWMCTL,
-//			CM_PASSWORD + CM_CLOCK_ENABLE + CM_SRC_OSCILLATOR);
 	bcm2835_write(CM_PWMCTL,
-			CM_PASSWORD + 0x16);
+			CM_PASSWORD | CM_CLOCK_ENABLE | CM_SRC_PLLDPER);
+//	bcm2835_write(CM_PWMCTL,
+//			CM_PASSWORD + 0x16);
 
 	/* Set the range of the PWM */
 	/*   0xc8 = 8-bit 48kHz */
@@ -90,10 +93,10 @@ uint32_t audio_pwm_init(void) {
 	bcm2835_write(PWM1_RANGE, 0x2c48);
 
 	/* Update the control register */
-	bcm2835_write(PWM_CONTROL,0x2161);
-//		PWM_CONTROL_USE_FIFO1 + PWM_CONTROL_ENABLE1 +
-//		PWM_CONTROL_USE_FIFO0 + PWM_CONTROL_ENABLE0 +
-//		PWM_CONTROL_CLEAR_FIFO);
+	bcm2835_write(PWM_CONTROL,				//0x2161);
+		PWM_CONTROL_USE_FIFO1 + PWM_CONTROL_ENABLE1 +
+		PWM_CONTROL_USE_FIFO0 + PWM_CONTROL_ENABLE0 +
+		PWM_CONTROL_CLEAR_FIFO);
 
 	/* Set PWM DMA Enable bit */
 	/* Bits 0..7 DMA Threshold For DREQ Signal = 1 */
@@ -107,16 +110,11 @@ uint32_t audio_pwm_init(void) {
 	bcm2835_write(DMA0_BASE+DMA_CONBLK_AD,
 		(uint32_t)&cb_struct1);
 
-	/* Start DMA */
-	bcm2835_write(DMA0_BASE+DMA_CS,DMA_CS_DMA_ACTIVE);
-
-	printk("Sound enabled\n");
-
 	return 0;
 }
 
 /* write the data */
-int32_t audio_pwm_write(uint8_t *buffer, uint32_t len) {
+int32_t audio_pwm_write(uint8_t *buffer, uint32_t len, int repeat) {
 
 #if 0
 	int i=0;
@@ -146,17 +144,29 @@ fifo_full:
 
 	return i;
 #endif
+
+	/* Start DMA */
+	bcm2835_write(DMA0_BASE+DMA_CS,DMA_CS_DMA_ACTIVE);
+
 	return 0;
 }
 
+	/* Disabled for now */
 int32_t audio_beep(void) {
 
-	uint32_t status;
+	uint32_t active;
 
-	status=bcm2835_read(PWM_STATUS);
-	audio_pwm_write(NULL,1000);
+//	status=bcm2835_read(PWM_STATUS);
+//	printk("Status: %x\n",status);
 
-	printk("Status: %x\n",status);
+	/* Start audio */
+	audio_pwm_write(NULL,1000,0);
+	delay(1000000);
+
+	/* Stop DMA */
+	active=bcm2835_read(DMA0_BASE+DMA_CS);
+	active&=~DMA_CS_DMA_ACTIVE;
+	bcm2835_write(DMA0_BASE+DMA_CS,active);
 
 	return 0;
 }
