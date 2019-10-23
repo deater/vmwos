085c8dda8f425876fe90657ee811a4ec35bb2be9
Vince Weaver <vince@deater.net>
2019-10-22 21:23:09 -0400

    userspace: demo: add doom fire

---
+++ userspace/demo/doom_fire.c
@@ -0,0 +1,90 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef VMWOS
+#include "syscalls.h"
+#include "vlibc.h"
+#include "vmwos.h"
+#else
+#include <stdio.h>
+#include <unistd.h>
+#endif
+
+#include "svmwgraph.h"
+#include "pi-graphics.h"
+#include "default_font.c"
+
+static struct palette pal;
+
+static unsigned char buffer[XSIZE*YSIZE];
+
+/* The state word must be initialized to non-zero */
+static uint32_t rand32(void) {
+	/* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
+	static uint32_t x = 0xfeb13;
+	x ^= x << 13;
+	x ^= x >> 17;
+	x ^= x << 5;
+	return x;
+}
+
+/* Based on Doom Fire as described by FABIEN SANGLARD */
+/* http://fabiensanglard.net/doom_fire_psx/ */
+
+int main(int argc, char **argv) {
+
+	int x,y,ch,newcol,r,dst;
+
+	int rgbs[38]={
+		0x000000,
+                0x070707,0x1F0707,0x2F0F07,0x470F07,
+		0x571707,0x671F07,0x771F07,0x8F2707,
+		0x9F2F07,0xAF3F07,0xBF4707,0xC74707,
+                0xDF4F07,0xDF5707,0xDF5707,0xD75F07,
+                0xD75F07,0xD7670F,0xCF6F0F,0xCF770F,
+                0xCF7F0F,0xCF8717,0xC78717,0xC78F17,
+		0xC7971F,0xBF9F1F,0xBF9F1F,0xBFA727,
+		0xBFA727,0xBFAF2F,0xB7AF2F,0xB7B72F,
+		0xB7B737,0xCFCF6F,0xDFDF9F,0xEFEFC7,
+                0xFFFFFF,
+	};
+
+	pi_graphics_init();
+
+	vmwClearScreen(0,buffer);
+
+	for(x=0;x<38;x++) {
+		pal.red[x]=(rgbs[x]>>16)&0xff;
+		pal.green[x]=(rgbs[x]>>8)&0xff;
+		pal.blue[x]=(rgbs[x])&0xff;
+	}
+
+	/* draw white line at bottom */
+	vmwHlin(0,640,479,37,buffer);
+
+//	vmwTextXYx2("A VMW SOFTWARE PRODUCTION",60*2,140*2,
+//			15,15,0,default_font,buffer);
+
+	while(1) {
+		for(x=0;x<640;x++) {
+			for(y=200;y<479;y++) {
+				r=rand32()&7;
+				dst=(y*XSIZE)+x-(r&3)+1;
+//				if (dst>=640*480) dst=(640*480)-1;
+				newcol=buffer[((y+1)*XSIZE)+x]-(r<2);
+				if (newcol<0) newcol=0;
+				buffer[dst]=newcol;
+			}
+		}
+
+		ch=pi_graphics_input();
+		if (ch=='q') break;
+		if (ch==27) break;
+		pi_graphics_update(buffer,&pal);
+		usleep(10000);
+	}
+
+	return 0;
+}
+
+
