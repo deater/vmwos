009a746bb69656fcb01bcce5d66866c72135dd05
Vince Weaver <vincent.weaver@maine.edu>
2020-01-08 11:21:55 -0500

    userspace: editor: up to step 100 of the tutorial

--- userspace/editor/nano.c
+++ userspace/editor/nano.c
@@ -4,6 +4,7 @@
 /* Modified to be more similar to the nano editor */
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <termios.h>
 #include <ctype.h>
@@ -11,8 +12,10 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
+#include <time.h>
 
 #define VMW_NANO_VERSION "0.0.1"
+#define NANO_TAB_STOP	8
 
 enum editor_keys {
 	ARROW_LEFT = 0x1000,
@@ -28,17 +31,25 @@ enum editor_keys {
 
 struct editor_row {
 	int size;
+	int rsize;
 	char *chars;
+	char *render;
 };
 
 struct editor_config {
-	struct termios orig_termios;
 	int screenrows;
 	int screencols;
 	int cx,cy;
+	int rx;		/* rendered X location */
 	int rowoff;
+	int coloff;
 	int numrows;
 	struct editor_row *row;
+	char *filename;
+	char statusmsg[80];
+	time_t statusmsg_time;
+	struct termios orig_termios;
+
 };
 
 static struct editor_config config;
@@ -125,6 +136,43 @@ static void safe_exit(int value, char *string) {
 	exit(value);
 }
 
+static int editor_row_cx_to_rx(struct editor_row *row, int cx) {
+	int rx=0;
+	int j;
+
+	for(j=0;j<cx;j++) {
+		if (row->chars[j]=='\t') {
+			rx+=(NANO_TAB_STOP-1)-(rx%NANO_TAB_STOP);
+		}
+		rx++;
+	}
+	return rx;
+}
+
+static void editor_update_row(struct editor_row *row) {
+	int tabs = 0;
+	int j,idx=0;
+
+	for(j=0;j<row->size;j++) {
+		if (row->chars[j]=='\t') tabs++;
+	}
+
+	free(row->render);
+	row->render = malloc(row->size+ tabs*(NANO_TAB_STOP-1) + 1);
+
+	for(j=0;j<row->size;j++) {
+		if (row->chars[j]=='\t') {
+			row->render[idx++] = ' ';
+			while (idx % NANO_TAB_STOP != 0) row->render[idx++] = ' ';
+		}
+		else {
+			row->render[idx++] = row->chars[j];
+		}
+	}
+	row->render[idx]='\0';
+	row->rsize=idx;
+
+}
 
 static void editor_append_row(char *s, size_t len) {
 
@@ -137,6 +185,11 @@ static void editor_append_row(char *s, size_t len) {
 	config.row[at].chars=malloc(len+1);
 	memcpy(config.row[at].chars,s,len);
 	config.row[at].chars[len]='\0';
+
+	config.row[at].rsize=0;
+	config.row[at].render=NULL;
+	editor_update_row(&config.row[at]);
+
 	config.numrows++;
 }
 
@@ -146,6 +199,9 @@ static void editor_open(char *filename) {
 	char *line=NULL;
 	size_t linecap=0;
 	ssize_t linelen;
+
+	free(config.filename);
+	config.filename=strdup(filename);
 
 	fff=fopen(filename,"r");
 	if (!fff) {
@@ -226,15 +282,26 @@ static int editor_read_key(void) {
 
 static void editor_move_cursor(int key) {
 
+	struct editor_row *row;
+	int rowlen;
+
+	row=(config.cy>config.numrows)?NULL:&config.row[config.cy];
+
 	switch(key) {
 		case ARROW_LEFT:
 			if (config.cx!=0) {
 				config.cx--;
+			} else if (config.cy>0) {
+				config.cy--;
+				config.cx=config.row[config.cy].size;
 			}
 			break;
 		case ARROW_RIGHT:
-			if (config.cx!=config.screencols-1) {
+			if ( (row) && (config.cx < row->size)) {
 				config.cx++;
+			} else if ( (row) && (config.cx == row->size)) {
+				config.cy++;
+				config.cx=0;
 			}
 			break;
 		case ARROW_UP:
@@ -247,6 +314,13 @@ static void editor_move_cursor(int key) {
 				config.cy++;
 			}
 			break;
+	}
+
+	/* If we move to a new line, be sure we aren't off the end */
+	row=(config.cy >= config.numrows) ? NULL:&config.row[config.cy];
+	rowlen=row?row->size:0;
+	if (config.cx > rowlen) {
+		config.cx = rowlen;
 	}
 }
 
@@ -265,10 +339,20 @@ static void editor_process_key(void) {
 			config.cx=0;
 			break;
 		case END_KEY:
-			config.cx=config.screencols-1;
+			if (config.cy<config.numrows) {
+				config.cx=config.row[config.cy].size;
+			}
 			break;
 		case PAGE_UP:
 		case PAGE_DOWN:
+			if (c==PAGE_UP) {
+				config.cy=config.rowoff;
+			}
+			else if (c==PAGE_DOWN) {
+				config.cy=config.rowoff+config.screenrows-1;
+				if (config.cy>config.numrows) config.cy=config.numrows;
+			}
+
 			times=config.screenrows;
 			while(times--)
 				editor_move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
@@ -311,21 +395,27 @@ static void editor_draw_rows(struct abuf *ab) {
 				abuf_append(ab,"~",1);
 			}
 		} else {
-			len=config.row[filerow].size;
+			len=config.row[filerow].rsize-config.coloff;
+			if (len<0) len=0;
 			if (len>config.screencols) len=config.screencols;
-			abuf_append(ab, config.row[filerow].chars,len);
+			abuf_append(ab, &config.row[filerow].render[config.coloff],len);
 
 		}
 		/* clear to end of line */
 		abuf_append(ab,"\x1b[K",3);
 
-		if (y<config.screenrows-1) {
-			abuf_append(ab,"\r\n",2);
-		}
+		abuf_append(ab,"\r\n",2);
+
 	}
 }
 
 static void editor_scroll(void) {
+
+	config.rx=0;
+
+	if (config.cy < config.numrows) {
+		config.rx=editor_row_cx_to_rx(&config.row[config.cy],config.cx);
+	}
 
 	/* check if cursor above visible window */
 	if (config.cy < config.rowoff) {
@@ -335,6 +425,53 @@ static void editor_scroll(void) {
 	/* check if cursor below visible window */
 	if (config.cy>=config.rowoff+config.screenrows) {
 		config.rowoff=config.cy-config.screenrows+1;
+	}
+
+	if (config.rx < config.coloff) {
+		config.coloff=config.rx;
+	}
+	if (config.rx >= config.coloff + config.screencols) {
+		config.coloff = config.rx - config.screencols+1;
+	}
+}
+
+static void editor_draw_status_bar(struct abuf *ab) {
+
+	int len=0,rlen;
+	char status[80],rstatus[80];
+
+	/* make it inverted text */
+	abuf_append(ab,"\x1b[7m",4);
+
+	len=snprintf(status,sizeof(status),"%.20s - %d lines",
+		config.filename?config.filename:"[No Name]",config.numrows);
+	rlen=snprintf(rstatus,sizeof(rstatus),"%d/%d",
+		config.cy+1,config.numrows);
+
+	if (len>config.screencols) len=config.screencols;
+	abuf_append(ab, status, len);
+
+	while(len< config.screencols) {
+		if (config.screencols-len==rlen) {
+			abuf_append(ab,rstatus,rlen);
+			break;
+		} else {
+			abuf_append(ab," ",1);
+			len++;
+		}
+	}
+	abuf_append(ab,"\x1b[m",3);
+	abuf_append(ab,"\r\n",2);
+
+}
+
+static void editor_draw_message_bar(struct abuf *ab) {
+	int msglen;
+	abuf_append(ab,"\x1b[K",3);
+	msglen=strlen(config.statusmsg);
+	if (msglen>config.screencols) msglen=config.screencols;
+	if ( (msglen) && (time(NULL) - config.statusmsg_time<5)) {
+		abuf_append(ab,config.statusmsg,msglen);
 	}
 }
 
@@ -354,10 +491,13 @@ static void editor_refresh_screen(void) {
 
 	editor_draw_rows(&ab);
 
+	editor_draw_status_bar(&ab);
+	editor_draw_message_bar(&ab);
+
 	/* move to x,y */
 	snprintf(buf, sizeof(buf), "\x1b[%d;%dH",
-		(config.cy+1-config.rowoff),
-		config.cx+1);
+		(config.cy-config.rowoff)+1,
+		(config.rx-config.coloff)+1);
 	abuf_append(&ab, buf, strlen(buf));
 
 	/* un-hide cursor */
@@ -369,6 +509,14 @@ static void editor_refresh_screen(void) {
 
 }
 
+static void editor_set_status_message(const char *fmt, ...) {
+
+	va_list ap;
+	va_start(ap,fmt);
+	vsnprintf(config.statusmsg, sizeof(config.statusmsg), fmt, ap);
+	va_end(ap);
+	config.statusmsg_time = time(NULL);
+}
 
 static int get_cursor_position(int *rows, int *cols) {
 
@@ -425,14 +573,22 @@ static void editor_init(void) {
 
 	config.cx=0;
 	config.cy=0;
+	config.rx=0;
 	config.rowoff=0;
+	config.coloff=0;
 	config.numrows=0;
 	config.row=NULL;
+	config.filename=NULL;
+	config.statusmsg[0]='\0';
+	config.statusmsg_time=0;
 
 	result=get_window_size(&config.screenrows,&config.screencols);
 	if (result==-1) {
 		safe_exit(1,"Error getting window size");
 	}
+
+	/* Make room for status bar */
+	config.screenrows-=2;
 }
 
 int main(int argc, char **argv) {
@@ -443,6 +599,8 @@ int main(int argc, char **argv) {
 	if (argc>=2) {
 		editor_open(argv[1]);
 	}
+
+	editor_set_status_message("HELP: ^X = quit");
 
 	while(1) {
 		editor_refresh_screen();
