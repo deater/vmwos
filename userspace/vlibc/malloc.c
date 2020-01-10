3a052d7c213508cb841e2da119c2c332863f366d
Vince Weaver <vincent.weaver@maine.edu>
2020-01-10 14:17:59 -0500

    userspace: vlibc: add malloc stubs for now

---
+++ userspace/vlibc/malloc.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stddef.h>
+
+void *malloc(uint32_t size) {
+	return NULL;
+}
+
+void free(void *ptr) {
+	return;
+}
+
+void *calloc(uint32_t nmemb, uint32_t size) {
+	return NULL;
+}
+
+void *realloc(void *ptr, uint32_t size) {
+	return NULL;
+}
+
+
