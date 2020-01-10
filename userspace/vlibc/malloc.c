46be56061660a3b2a917cc748aefeece424edec9
Vince Weaver <vincent.weaver@maine.edu>
2020-01-10 15:00:28 -0500

    vmwos: vlibc: add strdup()

--- userspace/vlibc/malloc.c
+++ userspace/vlibc/malloc.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include "string.h"
+
 void *malloc(uint32_t size) {
 	return NULL;
 }
@@ -17,4 +19,18 @@ void *realloc(void *ptr, uint32_t size) {
 	return NULL;
 }
 
+char *strdup(const char *s) {
 
+	int64_t string_size;
+	char *new_string=NULL;
+
+	string_size=strlen(s);
+
+	new_string=malloc(string_size+1);
+	if (new_string!=NULL) {
+		memcpy(new_string,s,string_size);
+		new_string[string_size]='\0';
+	}
+
+	return new_string;
+}
