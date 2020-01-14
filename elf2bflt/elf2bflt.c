b6fbf39240e0daf3f9a7b01ab2917f484b6c85fe
Vince Weaver <vincent.weaver@maine.edu>
2020-01-14 16:24:34 -0500

    userspace: have gcc/elf2bflt built relocatable executables

--- elf2bflt/elf2bflt.c
+++ elf2bflt/elf2bflt.c
@@ -9,7 +9,7 @@
 #include <sys/mman.h>
 #include <arpa/inet.h>
 
-static int debug=0;
+static int debug=1;
 
 #define SHT_NULL		0x0 	// Section header entry unused
 #define SHT_PROGBITS		0x1	// Program data
@@ -66,11 +66,20 @@ static void print_usage(char *name) {
 
 }
 
+static int is_relocated(uint32_t addr,uint32_t *relocations, int reloc_count) {
+
+	int i;
+
+	for(i=0;i<reloc_count;i++) {
+		if (addr==relocations[i]) return 1;
+	}
+	return 0;
+}
 
 
 int main(int argc, char **argv) {
 
-	int fd,out,i;
+	int fd,out,i,j;
 	struct stat sb;
 	char *addr,*shptr,*string_pointer,*name;
 	uint32_t temp;
@@ -80,11 +89,12 @@ int main(int argc, char **argv) {
 	uint32_t shnum,shoff,shsize,strindex;
 
 	uint32_t bss_size=0,data_size=0,text_size=0;
-	uint32_t text_start=0,data_start=0,bss_start=0,bss_end=0;
-	uint32_t reloc_start=0,reloc_count=0;
+	uint32_t text_start=0,data_start=0,bss_start=0,bss_end=0,data_end=0;
+	uint32_t reloc_start=0,reloc_count=0,reloc_index=0;
 	uint32_t *relocations=NULL;
-//	uint32_t data_offset=0,text_offset=0,bss_offset=0;
-	uint32_t entry=0,size,offset;
+	uint32_t temp_addr,temp_type;
+	uint32_t text_offset=0;//data_offset=0,bss_offset=0;
+	uint32_t entry=0,size,offset,output_addr;
 
 	if (argc<3) {
 		print_usage(argv[0]);
@@ -186,8 +196,14 @@ int main(int argc, char **argv) {
 
 	string_pointer=&addr[temp];
 
-	/* get sizes */
+	/***************************/
+	/* Get Sizes               */
+	/***************************/
+	/* FIXME: this is probably not doing the best job */
+
 	for(i=0;i<shnum;i++) {
+		if (debug) printf("Section header %d\n",i);
+
 		memcpy(&temp,&shptr[0x4],4);
 		if (debug) {
 			printf("\ttype: ");
@@ -196,7 +212,6 @@ int main(int argc, char **argv) {
 		}
 
 		if (temp==SHT_PROGBITS) {
-			if (debug) printf("Section header %d\n",i);
 
 			memcpy(&temp,&shptr[0x0],4);
 			name=string_pointer+temp;
@@ -207,13 +222,26 @@ int main(int argc, char **argv) {
 			else if (!strncmp(name,".text",6)) {
 				memcpy(&temp,&shptr[0x10],4);
 				offset=temp;
+				text_offset=offset;
 
 				memcpy(&temp,&shptr[0x14],4);
-				text_size=temp;
+				text_size+=temp;
 
 				if (debug) {
-					printf("\ttext_start: %x\n",text_start);
-					printf("\ttext_size: %x\n",text_size);
+					printf("\t.text at 0x%x size %d\n",
+						offset,temp);
+				}
+			}
+			else if (!strncmp(name,".text.startup",13)) {
+				memcpy(&temp,&shptr[0x10],4);
+				offset=temp;
+
+				memcpy(&temp,&shptr[0x14],4);
+				text_size+=temp;
+
+				if (debug) {
+					printf("\t.text.startup at 0x%x size %d\n",
+						offset,temp);
 				}
 			}
 			else if (!strncmp(name,".rodata",6)) {
@@ -224,9 +252,21 @@ int main(int argc, char **argv) {
 				text_size=(offset-entry)+temp;
 
 				if (debug) {
-					printf("\trodata_start: %x\n",offset);
-					printf("\ttext_size: %x\n",text_size);
+					printf("\t.rodata at 0x%x size %d\n",offset,temp);
 				}
+			}
+			else if (!strncmp(name,".interp",7)) {
+				memcpy(&temp,&shptr[0x10],4);
+				offset=temp;
+
+				memcpy(&temp,&shptr[0x14],4);
+				text_size+=temp;
+
+				if (debug) {
+					printf("\t.interp at 0x%x size %d\n",
+						offset,temp);
+				}
+
 			}
 			else if (!strncmp(name,".data.rel.local",15)) {
 
@@ -234,13 +274,11 @@ int main(int argc, char **argv) {
 				offset=temp;
 
 				memcpy(&temp,&shptr[0x14],4);
-				data_size=temp;
-
-				reloc_count+=data_size/4;
+				data_size+=temp;
 
 				if (debug) {
-					printf("Relocatable data at %x size %d\n",
-						offset,data_size);
+					printf(".data.rel.local at 0x%x size %d (total %d)\n",
+						offset,temp,data_size);
 				}
 			}
 			else if (!strncmp(name,".data.rel.ro.local",18)) {
@@ -249,12 +287,24 @@ int main(int argc, char **argv) {
 				offset=temp;
 
 				memcpy(&temp,&shptr[0x14],4);
-				data_size=temp;
-				reloc_count+=data_size/4;
+				data_size+=temp;
 
 				if (debug) {
-					printf("Read-only Relocatable data at %x size %d\n",
-						offset,data_size);
+					printf("\t.data.rel.ro.local at 0x%x size %d (total %d)\n",
+						offset,temp,data_size);
+				}
+			}
+			else if (!strncmp(name,".got.plt",8)) {
+
+				memcpy(&temp,&shptr[0x10],4);
+				offset=temp;
+
+				memcpy(&temp,&shptr[0x14],4);
+				data_size+=temp;
+
+				if (debug) {
+					printf("\t.got.plt at 0x%x size %d (total %d)\n",
+						offset,temp,data_size);
 				}
 			}
 			else if (!strncmp(name,".data",6)) {
@@ -262,17 +312,14 @@ int main(int argc, char **argv) {
 				offset=temp;
 
 				memcpy(&temp,&shptr[0x14],4);
-				data_size=temp;
+				data_size+=temp;
 
 				data_start=offset-entry;
 				if (debug) {
-					printf("\tdata_start: %x, %x %d\n",
-						data_start,offset,data_size);
+					printf("\t.data [0x%x] at 0x%x size %d (total %d)\n",
+						data_start,offset,temp,data_size);
 				}
 
-			}
-			else if (!strncmp(name,".text.startup",13)) {
-				/* what to do with this? */
 			}
 			else {
 				fprintf(stderr,"ERROR: what to do with %s\n",name);
@@ -310,8 +357,69 @@ int main(int argc, char **argv) {
 	}
 
 	/***************************/
+	/* Create the Reloc Table  */
+	/***************************/
+
+	if (debug) printf("** CREATING RELOC TABLE **\n");
+
+	shptr=&addr[shoff];
+
+	for(i=0;i<shnum;i++) {
+
+		memcpy(&temp,&shptr[0x4],4);
+
+		if (temp==SHT_REL) {
+
+			if (debug) printf("Section header %d\n",i);
+
+			memcpy(&temp,&shptr[0x0],4);
+			name=string_pointer+temp;
+
+			if (!strncmp(name,".rel.dyn",8)) {
+				memcpy(&temp,&shptr[0x10],4);
+				offset=temp;
+
+				memcpy(&temp,&shptr[0x14],4);
+				size=temp;
+
+				relocations=calloc(size/8,sizeof(uint32_t));
+				if (relocations==NULL) {
+					fprintf(stderr,"ERROR ALLOCATING\n");
+					exit(6);
+				}
+				reloc_count=size/8;
+
+				if (debug) {
+					printf("\t.rel.dyn at 0x%x size %d\n",
+						offset,size);
+				}
+				printf("Trying up to %d\n",size/8);
+				for(j=0;j<size;j+=8) {
+					temp_addr=*(uint32_t *)(&addr[offset+j]);
+					temp_type=*(uint32_t *)(&addr[offset+j]+4);
+					if (temp_type!=0x17) {
+						fprintf(stderr,"Unknown reloc type 0x%x\n",temp_type);
+						exit(5);
+					}
+					relocations[reloc_index]=temp_addr;
+					reloc_index++;
+
+					if (debug) {
+						printf("%d: %x %x\n",j,
+							temp_addr,temp_type);
+					}
+				}
+
+			}
+		}
+		shptr+=shsize;
+	}
+
+	/***************************/
 	/* Write out the bflt file */
 	/***************************/
+
+	if (debug) printf("** WRITING BFLT FILE **\n");
 
 	text_start=0x40;
 	if (data_start==0) {
@@ -319,6 +427,8 @@ int main(int argc, char **argv) {
 	} else {
 		data_start+=text_start;
 	}
+
+	data_end=data_start+data_size;
 
 	if (bss_start==0) {
 		bss_start=data_start+data_size;
@@ -331,11 +441,6 @@ int main(int argc, char **argv) {
 
 	if (reloc_count) {
 		reloc_start=bss_start;
-		relocations=calloc(reloc_count,sizeof(uint32_t));
-		if (relocations==NULL) {
-			fprintf(stderr,"Error allocating allocation space\n");
-			return 5;
-		}
 	}
 
 	/* magic */
@@ -356,7 +461,11 @@ int main(int argc, char **argv) {
 	write(out,&temp,4);
 
 	/* data_end */
-	if (debug) printf("BFLT: data_end %x\n",bss_start);
+	if (debug) printf("BFLT: data_end %x (%x+%d)\n",data_end,
+		data_start,data_size);
+
+	/* bss_start */
+	if (debug) printf("BFLT: bss_start %x\n",bss_start);
 	temp=htonl(bss_start);
 	write(out,&temp,4);
 
@@ -392,6 +501,7 @@ int main(int argc, char **argv) {
 	shptr=&addr[shoff];
 	if (debug) printf("Writing data:\n");
 	for(i=0;i<shnum;i++) {
+		printf("SHNUM %d/%d\n",i,shnum);
 		memcpy(&temp,&shptr[0x4],4);
 		if (temp==SHT_PROGBITS) {
 			if (debug) printf("Section header %d\n",i);
@@ -401,6 +511,7 @@ int main(int argc, char **argv) {
 
 			/* Don't write out comment */
 			if (!strcmp(name,".comment")) {
+				if (debug) printf("Skipping .comment\n");
 			}
 			else if ((!strncmp(name,".text",5)) ||
 				 (!strncmp(name,".rodata",6))) {
@@ -419,8 +530,10 @@ int main(int argc, char **argv) {
 				lseek(out,text_start+offset-entry,SEEK_SET);
 				write(out,&addr[offset],size);
 			}
-			else if (!strncmp(name,".data",5)) {
-				if (debug) printf("Writing data: %s\n",name);
+			else if (!strncmp(name,".interp",7)) {
+				/* interpreter, probaby something like */
+				/* /usr/lib/ld.so.1 */
+				if (debug) printf("Writing useless data: %s\n",name);
 
 				memcpy(&temp,&shptr[0x10],4);
 				offset=temp;
@@ -434,8 +547,38 @@ int main(int argc, char **argv) {
 					text_start+offset-entry);
 				lseek(out,text_start+offset-entry,SEEK_SET);
 				write(out,&addr[offset],size);
+			}
+			else if ((!strncmp(name,".data.rel.local",15)) ||
+				(!strncmp(name,".data.rel.ro.local",18)) ||
+				(!strncmp(name,".got.plt",8)) ||
+				(!strncmp(name,".data",5))) {
 
+				if (debug) printf("Writing data: %s\n",name);
 
+				memcpy(&temp,&shptr[0x10],4);
+				offset=temp;
+				if (debug) printf("\toffset: %x\n",offset);
+
+				memcpy(&temp,&shptr[0x14],4);
+				size=temp;
+				if (debug) printf("\tsize: %x\n",size);
+
+				output_addr=text_start+offset-entry;
+
+				if (debug) printf("Seeking to %x\n",
+					output_addr);
+				lseek(out,output_addr,SEEK_SET);
+
+				output_addr=offset;
+				for(j=0;j<size;j+=4) {
+					temp=*(uint32_t *)&addr[offset+j];
+					printf("TESTING RELOC %x\n",output_addr+j);
+					if (is_relocated(output_addr+j,relocations,reloc_count)) {
+						if (debug) printf("Relocating %x\n",output_addr+j);
+						temp=temp-text_offset;
+					}
+					write(out,&temp,sizeof(uint32_t));
+				}
 			}
 			else {
 				printf("What to do with %s\n",name);
@@ -447,9 +590,22 @@ int main(int argc, char **argv) {
 		shptr+=shsize;
 	}
 
-
-
 	munmap(addr, sb.st_size);
+
+	/* Write out relocations */
+
+	/* FIXME: relocate GOT too */
+	if (reloc_count) {
+		lseek(out,reloc_start,SEEK_SET);
+		for(j=0;j<reloc_count;j++) {
+			if (debug) printf("reloc %d: old %x new %x\n",
+				j,relocations[j],relocations[j]-text_offset);
+			temp_addr=htonl(relocations[j]-text_offset);
+			write(out,&temp_addr,sizeof(uint32_t));
+		}
+	}
+
+	if (relocations) free(relocations);
 
 	close(fd);
 	close(out);
