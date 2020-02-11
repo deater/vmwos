bb99bae8cc27f655475e49f24d4084eed755e2a1
Vince Weaver <vincent.weaver@maine.edu>
2020-02-10 21:25:08 -0500

    userspace: add truncate utility

---
+++ userspace/truncate.c
@@ -0,0 +1,52 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef VMWOS
+#include "syscalls.h"
+#include "vlibc.h"
+#include "vmwos.h"
+#else
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#endif
+
+//static int debug=1;
+
+static void usage(void) {
+	printf("\nUsage: truncate [-s size] FILENAME\n\n");
+	return;
+}
+
+int main(int argc, char **argv) {
+
+	int c,size=0,result;
+
+	opterr=0;
+	while ((c=getopt(argc,argv,"hs"))!=-1) {
+		switch(c) {
+			case 's':
+				size=atoi(optarg);
+				break;
+			case 'h':
+			default:
+				usage();
+				break;
+		}
+	}
+
+	if (optind<argc) {
+		printf("Truncating %s to size %d\n",argv[optind],size);
+
+		result=truncate(argv[optind],size);
+		if (result<0) {
+			printf("Error truncating: %s\n",strerror(errno));
+		}
+	}
+	else {
+		usage();
+	}
+	return 0;
+}
--- userspace/vlibc/include/syscalls.h
+++ userspace/vlibc/include/syscalls.h
@@ -99,6 +99,7 @@ int32_t read(int fd, void *buf, size_t count);
 int32_t open(const char *filename, uint32_t flags, uint32_t mode);
 int32_t close(uint32_t fd);
 int32_t ftruncate(int32_t fd, int64_t length);
+int32_t truncate(const char *path, int64_t length);
 
 #define SEEK_SET	0	/* Seek from beginning of file	*/
 #define SEEK_CUR	1	/* Seek from current position	*/
