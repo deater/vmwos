d716237db0623396dfce0239db726588b6f48066
Vince Weaver <vincent.weaver@maine.edu>
2023-10-21 00:27:54 -0400

    boot: hardware_detect: update with current model list

--- kernel/boot/hardware_detect.c
+++ kernel/boot/hardware_detect.c
@@ -12,7 +12,7 @@ static int32_t atags_found=0;
 
 /* default, this is over-ridden later */
 //uint32_t hardware_type=RPI_MODEL_B;
-static uint32_t hardware_type=RPI_MODEL_3B;
+uint32_t hardware_type=RPI_MODEL_3B;
 static uint32_t hardware_revision;
 
 
@@ -37,32 +37,67 @@ static uint32_t hardware_convert_type(uint32_t revision) {
 		case 0x9:	type=RPI_MODEL_A;
 				break;
 		case 0x10:
-		case 0x13:	type=RPI_MODEL_BPLUS;
+		case 0x13:
+		case 0x900032:
+				type=RPI_MODEL_BPLUS;
 				break;
 		case 0x11:
-		case 0x14:	type=RPI_COMPUTE_NODE;
+		case 0x14:
+				type=RPI_COMPUTE_MODULE1;
+				break;
+		case 0xa020a0:
+				type=RPI_COMPUTE_MODULE3;
 				break;
 		case 0x12:
-		case 0x15:	type=RPI_MODEL_APLUS;
+		case 0x15:
+		case 0x900021:	/* 512 MB */
+				type=RPI_MODEL_APLUS;
 				break;
-		case 0x90092:
-		case 0x90093:
+		case 0x900092:
+		case 0x900093:
+		case 0x920093:
 				type=RPI_MODEL_ZERO;
+				break;
+		case 0x9000c1:
+				type=RPI_MODEL_ZERO_W;
+				break;
+		case 0x902120:
+				type=RPI_MODEL_ZERO_2W;
 				break;
 		case 0xa01040:
 		case 0xa01041:
 		case 0xa21041:
-		case 0xa22042:
 				type=RPI_MODEL_2B;
+				break;
+		case 0xa22042:
+				type=RPI_MODEL_2B_V1_2;
 				break;
 		case 0xa02082:
 		case 0xa22082:
+		case 0xa32082:
 				type=RPI_MODEL_3B;
 				break;
 		case 0xa020d3:
 				type=RPI_MODEL_3BPLUS;
 				break;
-		default:	type=RPI_UNKNOWN;
+		case 0x9020e0:
+				type=RPI_MODEL_3APLUS;
+				break;
+		case 0xa03111:
+		case 0xb03111:
+		case 0xb03112:
+		case 0xb03114:
+		case 0xb03115:
+		case 0xc03111:
+		case 0xc03112:
+		case 0xc03114:
+		case 0xc03115:
+		case 0xd03114:
+		case 0xd03115:
+				type=RPI_MODEL_4B;
+				break;
+
+		default:	type=RPI_MODEL_UNKNOWN;
 				break;
 	}
 
@@ -105,17 +140,25 @@ void hardware_print_model(uint32_t version) {
 		printk("\tDevice Tree reports: %s\n",string);
 	}
 
-	printk("\tVMWos detects as: Model ");
+	printk("\tWe detect as: Model ");
 	switch(hardware_type) {
 		case RPI_MODEL_A:	printk("A"); break;
 		case RPI_MODEL_APLUS:	printk("A+"); break;
 		case RPI_MODEL_B:	printk("B"); break;
 		case RPI_MODEL_BPLUS:	printk("B+"); break;
 		case RPI_MODEL_2B:	printk("2B"); break;
+		case RPI_MODEL_2B_V1_2:	printk("2B v1.2"); break;
 		case RPI_MODEL_3B:	printk("3B"); break;
 		case RPI_MODEL_3BPLUS:	printk("3B+"); break;
+		case RPI_MODEL_3APLUS:	printk("3A+"); break;
 		case RPI_MODEL_ZERO:	printk("Zero"); break;
-		case RPI_COMPUTE_NODE:	printk("Compute Node"); break;
+		case RPI_MODEL_ZERO_W:	printk("Zero W"); break;
+		case RPI_MODEL_ZERO_2W:	printk("Zero 2W"); break;
+		case RPI_COMPUTE_MODULE1:
+					printk("Compute Module 1"); break;
+		case RPI_COMPUTE_MODULE3:
+					printk("Compute Module 3"); break;
+		case RPI_MODEL_4B:	printk("4B"); break;
 		default:		printk("Unknown %x",hardware_revision);
 					break;
 	}
@@ -161,3 +204,67 @@ void hardware_get_memory(uint32_t *start, uint32_t *length) {
 		*length=256*1024*1024;
 	}
 }
+
+
+extern uint32_t io_base;
+uint32_t act_led_gpio=0;
+
+void hardware_setup_vars(void) {
+
+	switch(hardware_type) {
+		case RPI_MODEL_A:
+			io_base=0x20000000;
+			act_led_gpio=16;
+			break;
+		case RPI_MODEL_APLUS:
+			io_base=0x20000000;
+			act_led_gpio=16;
+			break;
+		case RPI_MODEL_B:
+			io_base=0x20000000;
+			break;
+		case RPI_MODEL_BPLUS:
+			io_base=0x20000000;
+			break;
+		case RPI_MODEL_2B:
+			io_base=0x3f000000;
+			break;
+		case RPI_MODEL_2B_V1_2:
+			io_base=0x3f000000;
+			break;
+		case RPI_MODEL_3B:
+			io_base=0x3f000000;
+			break;
+		case RPI_MODEL_3BPLUS:
+			io_base=0x3f000000;
+			act_led_gpio=29;
+			break;
+		case RPI_MODEL_3APLUS:
+			io_base=0x3f000000;
+			act_led_gpio=29;
+			break;
+		case RPI_MODEL_ZERO:
+			io_base=0x20000000;
+			break;
+		case RPI_MODEL_ZERO_W:
+			io_base=0x20000000;
+			break;
+		case RPI_MODEL_ZERO_2W:
+			io_base=0x3f000000;
+			break;
+		case RPI_COMPUTE_MODULE1:
+			io_base=0x20000000;
+			break;
+		case RPI_COMPUTE_MODULE3:
+			io_base=0x3f000000;
+			break;
+		case RPI_MODEL_4B:
+			act_led_gpio=42;
+			io_base=0xfe000000;
+			break;
+		default:
+			io_base=0xfe000000;
+			break;
+	}
+}
+
--- kernel/include/boot/hardware_detect.h
+++ kernel/include/boot/hardware_detect.h
@@ -1,17 +1,27 @@
-#define RPI_UNKNOWN		0
+#define RPI_MODEL_UNKNOWN	0
 #define RPI_MODEL_A		1
 #define RPI_MODEL_B		2
 #define RPI_MODEL_APLUS		3
 #define RPI_MODEL_BPLUS		4
-#define RPI_COMPUTE_NODE	6
+#define RPI_COMPUTE_MODULE1	6
 #define RPI_MODEL_2B		7
 #define RPI_MODEL_ZERO		8
 #define RPI_MODEL_3B		9
 #define RPI_MODEL_3BPLUS	10
+#define	RPI_MODEL_2B_V1_2	11
+#define RPI_COMPUTE_MODULE3	12
+#define RPI_MODEL_ZERO_W	13
+#define RPI_MODEL_ZERO_2W	14
+#define RPI_MODEL_3APLUS	15
+#define RPI_MODEL_4B		16
 
 uint32_t hardware_detect(void *info_ptr);
 void hardware_print_model(uint32_t version);
 void hardware_print_commandline(void);
 uint32_t hardware_get_type(void);
 void hardware_get_memory(uint32_t *start, uint32_t *length);
+
+void hardware_setup_vars(void);
+
+extern uint32_t hardware_type;
 
